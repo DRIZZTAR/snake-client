d64ded4b9a985f60067b41a123cbfed495dd103b
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-09-15 20:47:15 -0600

    updating play and client with es6 shorthandsyntax, and export connect function into play.

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = () => {
+  const conn = net.createConnection({
+    host: "192.168.210.255",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // Handle events and interactions with the server here
+  conn.on("connect", () => {
+    console.log("Connected to the server!");
+    // You can start sending/receiving data here
+  });
+
+  // Handle incoming data from the server
+  conn.on("data", (data) => {
+    console.log("Received data from the server:", data);
+    // You can process or display the incoming data here
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
+
--- play.js
+++ play.js
@@ -1,23 +1,8 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "192.168.210.255",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // Handle events and interactions with the server here
-  conn.on("connect", () => {
-    console.log("Connected to the server!");
-    // You can start sending/receiving data here
-  });
-
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log("Connecting ...");
-const client = connect(); // Store the connection in a variable
+const conn = connect(); // Store the connection in a variable named conn
+
+
+
+
